953c6ccdfd7dacf778fc048716a7aaa5db5330ad
SnappierCape <ulriconava.main@gmail.com>
2026-06-11 11:22:20 +0000

    test(long_run): run a 256 epoch training

--- scripts/train.py
+++ scripts/train.py
@@ -36,11 +36,11 @@ def main():
     experiment_config.sampling_device="cuda" if torch.cuda.is_available() else "cpu"
     experiment_config.train_device="cuda" if torch.cuda.is_available() else "cpu"
     experiment_config.max_n_iters=None
-    experiment_config.max_n_frames=17_694_720
+    experiment_config.max_n_frames=35_389_440
     experiment_config.on_policy_collected_frames_per_batch=138_240
     experiment_config.on_policy_n_minibatch_iters=16
     experiment_config.on_policy_minibatch_size=8640
-    experiment_config.lr=1e-3
+    experiment_config.lr=5e-4
     experiment_config.parallel_collection=True
     experiment_config.on_policy_n_envs_per_worker=4
     experiment_config.evaluation_interval=1_382_400
